886cc8e2492098fd1f7b2b5ce5cc252b34bc2a2f
praveenthati <praveen.thati@dish.com>
2017-03-28 16:21:01 -0600

    add test methods

--- src/test/java/com/example/EndpointsControllerTest.java
+++ src/test/java/com/example/EndpointsControllerTest.java
@@ -83,7 +83,6 @@ public class EndpointsControllerTest {
     @Test
     public void testMathService() throws Exception {
 
-
         MathService service = new MathService();
 
         List<String> valuesList = new ArrayList<>();
