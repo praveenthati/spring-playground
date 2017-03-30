90b9df8bff26eabd4e2cc8d8e60f9ba830b20121
praveenthati <praveen.thati@dish.com>
2017-03-30 12:49:06 -0600

    add CookieController

---
+++ src/main/java/com/example/CookieController.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CookieController {
+
+    @GetMapping("/header")
+    public String getHeader(@RequestHeader String host) {
+        return host;
+    }
+
+    @GetMapping("/cookie")
+    public String getCookie(@CookieValue(name = "foo") String cookie) {
+        return cookie;
+    }
+}
