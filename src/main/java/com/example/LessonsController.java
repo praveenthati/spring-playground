df580850aa8b8fdbed7e10e6e4f008986973c8d4
praveenthati <praveen.thati@dish.com>
2017-04-03 16:40:51 -0600

    add lessonscontroller. didnt add tests yet

---
+++ src/main/java/com/example/LessonsController.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RestController
+@RequestMapping("/lessons")
+public class LessonsController {
+
+    private final LessonRepository repository;
+
+    public LessonsController(LessonRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public Iterable<Lesson> all() {
+        return this.repository.findAll();
+    }
+
+    @PostMapping("")
+    public Lesson create(@RequestBody Lesson lesson) {
+        return this.repository.save(lesson);
+    }
+
+}
