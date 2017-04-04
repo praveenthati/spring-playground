85ae072282bb375f599cddcf93a44d7fd0032f16
praveenthati <praveen.thati@dish.com>
2017-04-04 14:27:24 -0600

    updates to lessonscontroller

--- src/main/java/com/example/controller/LessonsController.java
+++ src/main/java/com/example/controller/LessonsController.java
@@ -2,21 +2,37 @@ package com.example.controller;
 
 import com.example.model.entity.Lesson;
 import com.example.model.entityrepository.LessonRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/lessons")
 public class LessonsController {
 
-    private LessonRepository repository;
+    private final LessonRepository repository;
 
     public LessonsController(LessonRepository repository) {
         this.repository = repository;
+    }
+
+    @GetMapping("/{id}")
+    public Lesson getLesson(@PathVariable Long id) {
+        return this.repository.findOne(id);
+    }
+
+    @PatchMapping("/{id}")
+    public Lesson updateLesson(@PathVariable Long id,@RequestBody Lesson body) {
+
+        // fetch lesson before updating
+        // because body might be for diff id
+        Lesson lesson = this.repository.findOne(id);
+        lesson.setTitle(body.getTitle());
+        return this.repository.save(lesson);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLesson(@PathVariable Long id) {
+        this.repository.delete(id);
     }
 
     @GetMapping("")
