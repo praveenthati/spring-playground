3e652ce3cb5add3e996bbc2df48098b032c4ad14
praveenthati <praveen.thati@dish.com>
2017-04-04 21:56:13 -0600

    unit 5 lesson 3

---
+++ src/test/java/com/example/LessonsControllerTest.java
@@ -0,0 +1,150 @@
+package com.example;
+
+import com.example.controller.LessonsController;
+import com.example.model.entity.Lesson;
+import com.example.model.entityrepository.LessonRepository;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.hibernate.service.spi.InjectService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.transaction.Transactional;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class LessonsControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    LessonRepository repository;
+
+    private Gson gson = new GsonBuilder().create();
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testDeleteLessons() throws Exception{
+
+        //check size 0
+        mvc.perform(get("/lessons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+
+        Lesson lesson = new Lesson();
+        lesson.setTitle("Test");
+
+        // add
+        mvc.perform(post("/lessons")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(lesson)))
+                .andExpect(status().isOk());
+
+        for(Lesson item:repository.findAll()){
+            // delete 1 . As its test, database table will be recreated
+            mvc.perform(delete("/lessons/"+item.getId())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        }
+
+
+
+        // check size 2
+        mvc.perform(get("/lessons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testGetLessons() throws Exception{
+
+        //check size 0
+        mvc.perform(get("/lessons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void testCreateLessons() throws Exception{
+
+        //check size 0
+        mvc.perform(get("/lessons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+
+        Lesson lesson = new Lesson();
+        lesson.setTitle("Test");
+
+        // add
+        mvc.perform(post("/lessons")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(lesson)))
+                .andExpect(status().isOk());
+
+        // check size 1
+        mvc.perform(get("/lessons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].title", is("Test")));
+
+        // add
+        mvc.perform(post("/lessons")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(lesson)))
+                .andExpect(status().isOk());
+
+        // check size 2
+        mvc.perform(get("/lessons")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[1].title", is("Test")));
+
+        for(Lesson item:repository.findAll()){
+            repository.delete(item.getId());
+        }
+
+    }
+}
